bdfb3c691df5cfc881ef80d22af4ce02a9fdb1b8
vishesh20155 <vishesh20155@iiitd.ac.in>
2024-02-12 17:22:02 +0530

    gcloud setup

---
+++ gcloud_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Allow ssh using password: https://stackoverflow.com/questions/59533620/how-to-connect-to-gcp-vm-instance-with-password-using-ssh
+## In /etc/ssh/sshd_config
+## change -> PasswordAuthentication yes
+## sudo service ssh restart
+## sudo passwd $USER
+sudo apt update
+sudo apt upgrade
+
+sudo apt install vim git python3-pip
